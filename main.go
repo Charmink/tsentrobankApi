b4a70e52a0075a3f020c1f0f8e9b5ee32b5bbd48
Charm1nk <mry2002@mail.ru>
2021-06-03 15:55:05 +0300

    Added concurrency http requests

--- main.go
+++ main.go
@@ -9,47 +9,51 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 type Valute struct {
-	Name string
+	Name  string
 	Value string
 }
 
 type ValuteInformation struct {
 	Valute *Valute
-	Date string
+	Date   string
 }
 
-func action(url string, valutesCourse map[string][]ValuteInformation){
+func action(url string, valutesCourse map[string][]ValuteInformation, mt *sync.Mutex) {
 	response, _ := http.Get(url)
+	defer response.Body.Close()
 	body, _ := io.ReadAll(response.Body)
 	reader := strings.NewReader(string(body))
 	decoder := xml.NewDecoder(reader)
 	decoder.CharsetReader = charset.NewReaderLabel
 	date := ""
-	for{
+	for {
 		token, err := decoder.Token()
-		if err != nil && err != io.EOF{
+		if err != nil && err != io.EOF {
 			panic(err)
 		}
-		if token == nil{
+		if token == nil {
 			break
 		}
 		switch tp := token.(type) {
 		case xml.StartElement:
-			if tp.Name.Local == "ValCurs"{
-				for _, atr := range tp.Attr{
-					if atr.Name.Local == "Date"{
+			if tp.Name.Local == "ValCurs" {
+				for _, atr := range tp.Attr {
+					if atr.Name.Local == "Date" {
 						date = atr.Value
 					}
 				}
-			}else if tp.Name.Local == "Valute"{
+			} else if tp.Name.Local == "Valute" {
 				var v Valute
 				decoder.DecodeElement(&v, &tp)
+				mt.Lock()
 				valutesCourse[v.Name] = append(valutesCourse[v.Name],
 					ValuteInformation{&v, date})
+				mt.Unlock()
 
 			}
 		}
@@ -57,23 +61,23 @@ func action(url string, valutesCourse map[string][]ValuteInformation){
 }
 
 type ValuteStats struct {
-	Name string
-	Min float64
-	Max float64
-	Mid float64
+	Name    string
+	Min     float64
+	Max     float64
+	Mid     float64
 	MaxDate string
 	MinDate string
 }
 
-func countStats(valuesInfo []ValuteInformation) ValuteStats{
+func countStats(valuesInfo []ValuteInformation) ValuteStats {
 	valStat := ValuteStats{Name: valuesInfo[0].Valute.Name, Min: math.MaxInt64, Max: math.MinInt64, Mid: 0}
-	for _, valute := range valuesInfo{
+	for _, valute := range valuesInfo {
 		value, _ := strconv.ParseFloat(strings.Replace(valute.Valute.Value, ",", ".", -1), 64)
 		if value < valStat.Min {
 			valStat.Min = value
 			valStat.MinDate = valute.Date
 		}
-		if value > valStat.Max{
+		if value > valStat.Max {
 			valStat.Max = value
 			valStat.MaxDate = valute.Date
 		}
@@ -83,16 +87,24 @@ func countStats(valuesInfo []ValuteInformation) ValuteStats{
 	return valStat
 }
 
-func main()  {
+func main() {
 	currentTime := time.Now()
+	var mt sync.Mutex
 	valutesCourse := make(map[string][]ValuteInformation)
+	var wg sync.WaitGroup
 	url := "http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req="
-	for i := 0; i <= 90; i ++{
-		oldDate := currentTime.AddDate(0,0,-i)
+	for i := 0; i <= 90; i++ {
+		oldDate := currentTime.AddDate(0, 0, -i)
 		stringDate := oldDate.Format("02/01/2006")
-		action(url + stringDate, valutesCourse)
+		wg.Add(1)
+		go func(url string) {
+			action(url+stringDate, valutesCourse, &mt)
+			wg.Done()
+
+		}(url)
 	}
-	for _, value := range valutesCourse{
+	wg.Wait()
+	for _, value := range valutesCourse {
 		result := countStats(value)
 		fmt.Printf("Valute: %s\n", result.Name)
 		fmt.Printf("Max Value: %f Date: %s\n", result.Max, result.MaxDate)
